c584e5ef40eecf3b9da3434ce1b2b28459692115
Patryk Palej <patrykpalej@gmail.com>
2024-03-09 14:15:44 +0100

    Numeric filters

--- pages/1_filters.py
+++ pages/1_filters.py
@@ -29,9 +29,29 @@ numeric_df = df.select_dtypes("number")
 
 for column in numeric_df.columns:
     with st.expander(f"Filter {column} column"):
-        st.markdown(column)
+        n_unique = df[column].nunique()
+        column_min = df[column].min()
+        column_max = df[column].max()
+        unique_values = df[column].unique()
+        if n_unique > 4:
+            selected_min, selected_max = st.slider(
+                f"Select range for {column}", min_value=column_min,
+                max_value=column_max, value=[column_min, column_max])
+        else:
+            selected_min = st.selectbox(f"Select min.", options=sorted(unique_values),
+                                        index=0)
+            selected_max = st.selectbox(f"Select max.", options=sorted(unique_values),
+                                        index=n_unique - 1)
 
+        use_nans = st.toggle("Use NaN values", value=True, key=f"{column} toggle")
 
+        df = df[((df[column] <= selected_max)
+                & (df[column] >= selected_min))
+                | (df[column].isna() if use_nans else False)]
+
+        st.session_state.df = df
+st.divider()
+st.dataframe(df)
 
 
 # if not hasattr(st.session_state, "data"):
