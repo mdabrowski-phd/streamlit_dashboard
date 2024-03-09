36b3ad3bc9d4df31cd85967b809f4c82d728a8ca
Patryk Palej <patrykpalej@gmail.com>
2024-03-09 15:44:26 +0100

    Grid example (and columns)

--- pages/0_test.py
+++ pages/0_test.py
@@ -1,20 +1,37 @@
 import streamlit as st
+from streamlit_extras.grid import grid
 
 
-output = st.selectbox("Wybierz wartość", options=["a", "b", "c"], index=0)
-st.markdown(output)
+left, right = st.columns(2)
+
+with left:
+    left.markdown("Left column")  # left.<...> and st.<...> are both correct
+    left.markdown("Left column 123")  # left.<...> and st.<...> are both correct
+
+with right:
+    selected = right.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
+    right.markdown(selected)
 
 
-slider_output = st.slider(
-    "Wybierz wartosć", min_value=0., step=0.001, max_value=10., value=[3., 5.],
-    key="slider 1")
+st.divider()
 
-st.markdown(slider_output)
+a, _, b = st.columns([2, 4, 3])
 
-st.color_picker("Wybierz kolor")
+with a:
+    value = a.toggle("Toggle")
+    st.markdown(value)
 
-logical = st.toggle("Toggle value", value=True)
-st.markdown(logical)
+with b:
+    values = b.multiselect("Multiselect", [1, 2, 3])
+    st.markdown(values)
 
-st.number_input("Wybierz liczbę:", min_value=1., max_value=4., step=0.1,
-                value=2.5)
+
+st.divider()
+
+my_grid = grid(2, [1, 2, 3], 1)
+
+for i in range(20):
+    cont = my_grid.container()
+    with cont:
+        cont.markdown(i)
+        cont.markdown(i*2)
